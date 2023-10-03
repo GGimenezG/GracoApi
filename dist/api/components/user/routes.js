37b0491be925a322b5ae818c6ba69870e8769dbc
GGimenezG <gg091195@gmail.com>
2023-10-03 09:22:58 -0300

    Update routes.js

--- dist/api/components/user/routes.js
+++ dist/api/components/user/routes.js
@@ -4,7 +4,7 @@
 //  * permission validation (e.g. ACL) or add component specific middleware. */
 Object.defineProperty(exports, "__esModule", { value: true });
 const controller_1 = require("./controller");
-const auth_1 = require("api/middleware/validations/auth");
+const auth_1 = require("../../middleware/validations/auth");
 const router = (app) => {
     const c = new controller_1.UserController();
     app.post('/registrar', c.Create);
